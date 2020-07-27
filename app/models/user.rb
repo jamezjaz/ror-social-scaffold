b80877fbeba5f4f034af150f8366b5c3dc402ef0
Jamezjaz <jamezjaz@gmail.com>
2020-07-27 11:40:46 -0500

    Adds validation tests to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { minimum: 5, maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX, message: 'The email is not valid' }
+  validates :password, presence: true, length: { minimum: 6, maximum: 32 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
