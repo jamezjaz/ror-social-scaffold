3beeb2a02d25cbcccf14608690ac3c66bc347ee4
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-07-27 22:47:59 +0500

    Adds Friendship Association Tests

---
+++ spec/models/friendship_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+RSpec.describe Friendship, type: :model do
+  before(:all) do
+    User.create!(name: 'James', password: '1234567', email: 'email@mail.ru')
+    User.create!(name: 'Muhammad', password: '1234567', email: 'emaial@ail.ru')
+  end
+  describe '#valides' do
+    it 'validates friendship instance' do
+      friendship = Friendship.new(user_id: 2, friendship_id: 1)
+      expect(friendship).to be_valid
+    end
+    it 'validates friendship instance' do
+      friendship = Friendship.new(user_id: 2, friendship_id: 1)
+      friendship.user_id = ''
+      expect(friendship).not_to be_valid
+    end
+  end
+end
