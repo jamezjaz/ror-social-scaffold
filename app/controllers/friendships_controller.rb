5a7a17aaceb90216322c0c13cfc4f29b69f90b17
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-07-28 22:18:09 +0500

    Updated Core Functionality Delete, Approve Send Request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,29 +2,25 @@
 
 class FriendshipsController < ApplicationController
   def create
-    # fail
-    puts "paramameters is #{params[:user_id]}"
-    @friendship = Friendship.new(friendship_id: params[:user_id])
-    @friendship.user_id = current_user.id
-    @friendship.status = 'Pending'
-    if @friendship.save
-      flash[:notice] = 'Friend Request Sent'
-      redirect_to users_path
-    else
-      flash[:notice] = 'Friend Request Not Sent'
-      redirect_to users_path
+    case params[:requestname]
+      when "Send"
+        send_request
+      when "Approve"
+        approve_request
+      when "Unfriend"
+        unfriend_request
     end
   end
 
-  def update
-    @friendship = Friendship.find(params[:id])
-    @friendship.update(status: true)
-    @user = Friendship.where(user_id: current_user.id, friendship_id: params[:user_id])
-    # if @user.save
-    #   flash[:notice] = 'Friend Request Accepted'
-    #   redirect_to users_path
-    # end
-  end
+  # def update
+  #   @friendship = Friendship.find(params[:id])
+  #   @friendship.update(status: true)
+  #   @user = Friendship.where(user_id: current_user.id, friendship_id: params[:user_id])
+  #   # if @user.save
+  #   #   flash[:notice] = 'Friend Request Accepted'
+  #   #   redirect_to users_path
+  #   # end
+  # end
 
   # def create
   #   @friendship =  Friendship.new
@@ -57,6 +53,46 @@ class FriendshipsController < ApplicationController
   #     redirect_to user_path(@friendship.user_id)
   #   end
   # end
+  #
+  def send_request
+    @friendship = Friendship.new(friendship_id: params[:user_id])
+    @friendship.user_id = current_user.id
+    @friendship.status = 'Pending'
+    if @friendship.save
+      flash[:notice] = 'Friend Request Sent'
+      redirect_to users_path
+    else
+      flash[:notice] = 'Friend Request Not Sent'
+      redirect_to users_path
+    end
+  end
+
+  def approve_request
+    @friendship = Friendship.where("friendship_id = #{current_user.id}"). where("user_id = #{params[:user_id]}")
+    @friendship.first.status = "Confirmed"
+    if @friendship.first.save
+      @friendship = Friendship.create!(user_id: current_user.id, friendship_id: params[:user_id], status: "Confirmed")
+      flash[:notice] = 'Friend Request Approved'
+      redirect_to users_path
+    else
+      flash[:notice] = 'Friend Request Not Approved'
+      redirect_to users_path
+    end
+  end
+
+  def unfriend_request
+    @friendship = Friendship.where("friendship_id = #{current_user.id}"). where("user_id = #{params[:user_id]}")
+       if @friendship.exists?
+         @friendship.first.destroy
+         flash[:notice] = 'Friend Unfriended'
+         redirect_to users_path
+       else
+         @friendship = Friendship.where("user_id = #{current_user.id}"). where("friendship_id = #{params[:user_id]}")
+         @friendship.first.destroy
+         flash[:notice] = 'Friend Unfriended'
+         redirect_to users_path
+    end
+  end
 end
 
 # rubocop:enable Style/IdenticalConditionalBranches
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,15 +4,18 @@ module UsersHelper
   end
 
   def friendship_status(user_id)
+    case_request  = nil
     if current_user.friend?(user_id)
-      # link_to 'Unfriend', friendship_path(user_id), method: :delete, class: 'friend_button'
+      link_to 'Unfriend', user_friendships_path(user_id, requestname: "Unfriend"), method: :post, class: 'friend_button'
     elsif current_user.pending_requests.include?(user_id)
-      # link_to 'Cancel request', friendship_path(user_id), method: :delete, class: 'friend_button'
+      link_to 'Cancel', user_friendships_path(user_id, requestname: "Unfriend"), method: :post, class: 'friend_button'
     elsif current_user.received_requests.include?(user_id)
-      # link_to 'Approve request', friendship_path(user_id), method: :put, class: 'friend_button'
-      # link_to 'Reject Request', class: 'friend_button'
+      # link_to 'Reject', user_friendships_path(user_id, requestname: "Unfriend"), method: :post, class: 'friend_button'
+      link_to 'Approve request', user_friendships_path(user_id, requestname: "Approve"), method: :post, class: 'friend_button'
+
     else
-      link_to 'Send Request', user_friendships_path(user_id), method: :post, class: 'friend_button'
+      case_request = "Send"
+      link_to 'Send Request', user_friendships_path(user_id, requestname: "Send"), method: :post, class: 'friend_button'
       # link_to 'Send Request', friendships_path(user_id), method: :post, class: 'friend_button'
     end
   end
