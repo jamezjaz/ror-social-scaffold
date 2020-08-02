edded7c6540cf4deaec1962442a4df6387565099
Jamezjaz <jamezjaz@gmail.com>
2020-08-02 16:57:00 -0500

    Correct rubocop errors

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def posts_errors
-    if @post.errors.full_messages.any?
-      content_tag( :p, ("Post could not be saved. #{@post.errors.full_messages.join('. ')}"), :class=>'errors')
-    end
+    return unless @post.errors.full_messages.any?
+
+    content_tag(:p, "Post could not be saved. #{@post.errors.full_messages.join('. ')}", class: 'errors')
   end
 end
